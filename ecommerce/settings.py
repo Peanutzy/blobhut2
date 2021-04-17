b2bb835dd59480336d835393060e42dd49c55760
Peanutzy <79523123+Peanutzy@users.noreply.github.com>
2021-04-17 18:47:01 +0800

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -141,7 +141,3 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import dj_database_url
 prod_db  =  dj_database_url.config(conn_max_age=500)
 DATABASES['default'].update(prod_db)
-
-
-
-django_heroku.settings(locals())
